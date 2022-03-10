a8fc59f7fd37dbc10d7250a0bc4d6fa7ff06c467
Chaitanya Pawaia <chaitanyapawaiaa@gmail.com>
2022-03-11 02:30:24 +0530

    third Commit

--- Space Wars/Main.py
+++ Space Wars/Main.py
@@ -6,7 +6,8 @@ import pygame
 pygame.font.init()
 
 screenwidth = 1500
-screenheight = 800
+screenheight = 750
+
 screen = pygame.display.set_mode([screenwidth, screenheight])
 pygame.display.set_caption("Space Wars")
 
@@ -16,11 +17,23 @@ pygame.display.set_caption("Space Wars")
 enemy_1 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_1.png"))
 enemy_2 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_2.png"))
 enemy_3 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_3.png"))
+enemy_4 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_4.png"))
+enemy_5 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_5.png"))
 
 #bullet
 enemy_bullet = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","enemy_bullet.png"))
 power_bullet = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","power_bullet.png"))
 bullet = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","bullet.png"))
+
+#enemy lasers
+laser_1 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_1.png"))
+laser_2 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_2.png"))
+laser_3 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_3.png"))
+laser_4 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_4.png"))
+laser_5 = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_5.png"))
+
+#Player laser
+player_laser = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","laser_myship.png"))
 
 #Player ship
 player_ship = pygame.image.load(os.path.join(r"C:\Users\Chaitanya\Documents\Python Scripts\Pygame\Space Wars\Assets\Images","myship.png"))
@@ -44,25 +57,62 @@ class Ship:
     def draw(self, window):
         screen.blit(self.ship_img, (self.x, self.y))
 
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        print(self.ship_img.get_height())
+        return self.ship_img.get_height()
+
+
+
 class Player(Ship):
     def __init__(self, x, y, health=100):
-        super().__init__(self, x, y, health)
+        super().__init__(x, y, health)
         self.ship_img = player_ship
-        self.bullet_img = bullet
+        self.laser_img = player_laser
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
+class Enemy(Ship):
+    ENEMY_MAP = {
+        "enemy1":(enemy_1, laser_1),
+        "enemy2":(enemy_2, laser_2),
+        "enemy3":(enemy_3, laser_3),
+        "enemy4":(enemy_4, laser_4),
+        "enemy5":(enemy_5, laser_5)
+    }
+
+    def __init__(self, x, y, enemy, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.ENEMY_MAP[enemy]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
 
 def main():
     run = True
     FPS = 60
     score = 0
+    level = 0
     lives = 6
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 70)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
     player_vel = 5
 
-    player = Player(300, 650)
+    player = Player(300, 500)
 
     clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         screen.blit(game_bg, (0,0))
@@ -74,26 +124,64 @@ def main():
         screen.blit(score_label, (15, 10))
         screen.blit(lives_label, (screenwidth - lives_label.get_width() - 15, 10))
 
+        for enemy in enemies:
+            enemy.draw(screen)
+
         player.draw(screen)
+
+        if lost:
+            lost_label = lost_font.render("You Lost!!!!", 1, (255,255,255))
+            screen.blit(lost_label, (screenwidth/2 - lost_label.get_width()/2, screenheight/2))
+
 
         pygame.display.update()
 
 
-
     while run:
         clock.tick(FPS)
+
         redraw_window()
+
+        if lives <= 0 or player.health <=0:
+            lost =  True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 5 :
+                run = False
+            else:
+                continue
+
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, screenwidth-100), random.randrange(-1500, -100), random.choice(["enemy1", "enemy2", "enemy3", "enemy4", "enemy5" ]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and Player.x - player_vel > 0 : #left
-            Player.x -= player_vel
-        if keys[pygame.K_RIGHT] and Player.x + player_vel + 50 < screenwidth: #right
-            Player.x += player_vel
+        if keys[pygame.K_LEFT] and player.x - player_vel > 0 : #left
+            player.x -= player_vel
+        if keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < screenwidth: #right
+            player.x += player_vel
+        if keys[pygame.K_UP] and player.y - player_vel > 0 : #up
+            player.y -= player_vel
+        if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < screenheight: #down
+            player.y += player_vel
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > screenheight:
+                lives -= 1
+                enemies.remove(enemy)
+
         
+        redraw_window()
 
 main()
 
